b0e1e4fb8378b586bbcd3c16b8f8178baad5d6e0
echo27713 <echooo@gmail.com>
2025-02-21 20:11:34 -0800

    replace BrowseRouter with HashRouter to fix a reloading error in production.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProjectDetailPage from "./pages/ProjectDetailPage";
 
 const App = () => {
   return (
-    // <Router basename="/portfolio">
-    <Router
+    // <Router
+    //   basename={
+    //     process.env.NODE_ENV === "production"
+    //       ? process.env.PUBLIC_URL
+    //       : "/portfolio" // ensure you include the leading slash if needed
+    //   }
+    // >
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //     <Route path="/projects/:id" element={<ProjectDetailPage />} />
+    //   </Routes>
+    // </Router>
+
+    <HashRouter
       basename={
-        process.env.NODE_ENV === "production"
-          ? process.env.PUBLIC_URL
-          : "/portfolio" // ensure you include the leading slash if needed
+        process.env.NODE_ENV === "production" ? process.env.PUBLIC_URL : "/"
       }
     >
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects/:id" element={<ProjectDetailPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
