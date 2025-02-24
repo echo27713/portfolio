10790940a188d648b718db309781d427b10c7891
echo27713 <echooo@gmail.com>
2025-02-23 17:10:37 -0800

    EmblaCarouselButtonControl Added. But it still requires some CSS refinement.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,26 +1,35 @@
-import React, { useEffect, useCallback } from "react";
+// import React, { useEffect, useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
 import "../css/embla.css";
 
 function EmblaCarousel({ list = [] }) {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
 
-  const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev();
-  }, [emblaApi]);
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
 
-  const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext();
-  }, [emblaApi]);
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
 
-  useEffect(() => {
-    if (emblaApi) {
-      console.log(emblaApi.slideNodes()); // Access API
-    }
-  }, [emblaApi]);
+  //what is this useEffect for?
+  //   useEffect(() => {
+  //     if (emblaApi) {
+  //       console.log(emblaApi.slideNodes()); // Access API
+  //     }
+  //   }, [emblaApi]);
 
   return (
-    <div className="embla">
+    <section className="embla">
       <div class="embla_viewport" ref={emblaRef}>
         <div className="embla__container">
           {list.map((f, index) => (
@@ -28,18 +37,28 @@ function EmblaCarousel({ list = [] }) {
               {f}
             </div>
           ))}
-          {/* <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div> */}
         </div>
       </div>
-      <button className="embla__prev" onClick={scrollPrev}>
-        Prev
-      </button>
-      <button className="embla__next" onClick={scrollNext}>
-        Next
-      </button>
-    </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : ""
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 export default EmblaCarousel;
